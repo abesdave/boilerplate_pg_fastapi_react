4d912fe19dd2e16b6f152ba45ea1b913dea0d4e0
David <David@MacBook-Pro.local>
2024-04-12 10:22:42 +0200

    based state using set state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
 import './App.scss'
-
+import { useState } from 'react'
+import TabButton from "../components/TabButton/TabButton"
 import Right from '../components/Right/Right'
 
 function App() {
+
+  let [dynamicState, setDynamicState] = useState<string>("Set dynamic state")
+
+  const onSelected = (value: string) => {
+    alert(value)
+    setDynamicState(value)
+  }
+
   return (
     <>
       <section>
@@ -13,7 +22,18 @@ function App() {
       <section>
         <div className="row bg-warning">
           <h2>Examples:</h2>
+          <menu>
+            <TabButton onSelected={() => onSelected('element one')}>
+              <p>Nested Component</p>
+            </TabButton>
+            <TabButton onSelected={() => onSelected('element two')}>
+              <p>Nested Component</p>
+            </TabButton>
+          </menu>
         </div>
+      </section>
+      <section className="row bg-primary">
+        dynamic content: {dynamicState}
       </section>
     </>
   )
